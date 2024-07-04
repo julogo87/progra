3bc839394a644f667ca3dca162d98247a37f9218
julogo87 <julogo87@hotmail.com>
2024-07-03 22:19:15 -0500

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var data = [
-        ['Flight', 'Date', 'ST', 'State', 'STD', 'STA', 'Best DT', 'Best AT', 'From', 'To', 'Reg.', 'Own / Sub', 'Delay', 'Pax(F/C/Y)'],
-        ['', '', '', '', '', '', '', '', '', '', '', '', '', '']
-    ];
-
     var container = document.getElementById('table');
     var hot = new Handsontable(container, {
-        data: data,
+        data: [],
         rowHeaders: true,
-        colHeaders: true,
+        colHeaders: ['Flight', 'Date', 'ST', 'State', 'STD', 'STA', 'Best DT', 'Best AT', 'From', 'To', 'Reg.', 'Own / Sub', 'Delay', 'Pax(F/C/Y)'],
         contextMenu: true,
         minSpareRows: 1,
         columns: [
@@ -18,6 +13,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.getElementById('dataForm').addEventListener('submit', function() {
         var table_data = hot.getData();
-        document.getElementById('table_data').value = JSON.stringify(table_data);
+        var headers = hot.getColHeader();
+        var json_data = table_data.map(row => {
+            var rowData = {};
+            headers.forEach((header, index) => {
+                rowData[header] = row[index];
+            });
+            return rowData;
+        });
+        document.getElementById('table_data').value = JSON.stringify(json_data);
     });
 });
