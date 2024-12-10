2aeb0c0f702e3873ecc2c04d8c8aa8442377d802
julogo87 <julogo87@hotmail.com>
2024-12-09 20:58:58 -0500

    Update script.js

--- templates/static/js/script.js
+++ templates/static/js/script.js
@@ -1,23 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var data = [
-        ['Flight', 'Date', 'ST', 'State', 'STD', 'STA', 'Best DT', 'Best AT', 'From', 'To', 'Reg.', 'Own / Sub', 'Delay', 'Pax(F/C/Y)'],
-        ['','','','','','','','','','','','','','']
-    ];
-
     var container = document.getElementById('table');
     var hot = new Handsontable(container, {
-        data: data,
+        data: [],
         rowHeaders: true,
-        colHeaders: true,
+        colHeaders: [
+            'N', 'Flight', 'Date', 'ST', 'State', 'STD', 'STA', 'Best DT', 'Best AT', 
+            'From', 'To', 'Reg.', 'Own / Sub', 'Delay', 'Pax(F/C/Y)', 'Crew', 'Notas', 'Tripadi'
+        ],
         contextMenu: true,
         minSpareRows: 1,
         columns: [
-            {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
+            {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, 
+            { data: 'crew', type: 'text' },    // Columna Crew
+            { data: 'notas', type: 'text' },   // Columna Notas
+            { data: 'tripadi', type: 'text' }  // Columna Tripadi
         ]
     });
 
     document.getElementById('dataForm').addEventListener('submit', function() {
         var table_data = hot.getData();
-        document.getElementById('table_data').value = JSON.stringify(table_data);
+        var headers = hot.getColHeader();
+        var json_data = table_data.map(row => {
+            var rowData = {};
+            headers.forEach((header, index) => {
+                rowData[header] = row[index];
+            });
+            return rowData;
+        });
+        document.getElementById('table_data').value = JSON.stringify(json_data);
     });
 });
